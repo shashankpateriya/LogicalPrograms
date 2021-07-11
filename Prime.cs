1916528a35aa75c83bb3ef75e754026946e836b4
Shashank <shashankpateriya4@gmail.com>
2021-07-11 16:55:40 +0530

    [shashank] Add: Program to check prime number

---
+++ Prime.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LogicalProblems
+{
+    public class Prime
+    {
+        public void PrimeNumber()
+        {
+            Console.WriteLine("enter number to check its prime or not : ");
+            int Num = Convert.ToInt32(Console.ReadLine());
+            int a = 0;
+            for (int i = 2; i <= Num; i++)
+            {
+                if (Num % i == 0)
+                {
+                    a = a + 1;
+                }
+            }
+            if (a == 2)
+                Console.WriteLine("not a Prime Number" + Num);
+            else
+                Console.WriteLine(" Prime Number" + Num);
+            Console.ReadKey();
+        }
+    }
+}
