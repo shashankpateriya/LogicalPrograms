e383a7f00fcdd7e530df92364dd29e2cf025cd49
Shashank <shashankpateriya4@gmail.com>
2021-07-11 16:56:48 +0530

    [shashank] Add: Check if the number is perfect number

---
+++ PerfectNumber.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LogicalProblems
+{
+    public class PerfectNumber
+    {
+        public void PerfectNum()
+        {
+            int sum = 0;
+            Console.Write("enter the Number");
+            int Number = Convert.ToInt32(Console.ReadLine());
+            for (int i = 1; i < Number; i++)
+            {
+                if (Number % i == 0)
+                {
+                    sum = sum + i;
+                }
+            }
+            if (sum == Number)
+                Console.WriteLine("Entered number is a perfect number");
+            else
+                Console.WriteLine(" Entered number is not a perfect number");
+            Console.ReadKey();
+        }
+    }
+}
