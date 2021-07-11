8a1102119ba12cb80f1469117dc5ce8d60a5daca
Shashank <shashankpateriya4@gmail.com>
2021-07-11 16:53:53 +0530

    [shashank] Add: Next number is sum of last two numbers

---
+++ Fibonacci.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LogicalProblems
+{
+    public class Fibonacci
+    {
+        public void Fibb(int len)
+        {
+
+            int a = 0, b = 1, c;
+            Console.WriteLine("{0},{1}", a, b);
+            for (int i = 2; i < len; i++)
+            {
+                c = a + b;
+                Console.WriteLine("{0}", c);
+                a = b;
+                b = c;
+            }
+            Console.ReadKey();
+        }
+    }
+}
