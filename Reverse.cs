a4692a6251b3f11d1fb35b2ce6595c7a4e005bf4
Shashank <shashankpateriya4@gmail.com>
2021-07-11 16:57:14 +0530

    [shashank] Add: Program to reverse the number

---
+++ Reverse.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LogicalProblems
+{
+    public class Reverse
+    {
+        public void Rev()
+        {
+            Console.WriteLine("Enter a Number");
+            int Num = Convert.ToInt32(Console.ReadLine());
+            int Reverse = 0;
+            while (Num > 0)
+            {
+                int remainder = Num % 10;
+                Reverse = (Reverse * 10) + remainder;
+                Num = Num / 10;
+            }
+            Console.WriteLine("Reverse No. is {0}", Reverse);
+            Console.ReadKey();
+        }
+    }
+}
