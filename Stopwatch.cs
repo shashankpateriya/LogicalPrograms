53dae3e16a57a1b84b78559cb18fd056799c8b6c
Shashank <shashankpateriya4@gmail.com>
2021-07-11 17:56:43 +0530

    [shashank] Add: Stopwatch Program

---
+++ Stopwatch.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LogicalProblems
+{
+    public class StopWatch
+    {
+        public static void SWatch()
+        {
+            int start = DateTime.Now.Second;
+            Console.WriteLine("Timer started, Press Enter to stop");
+            Console.ReadLine();
+            int stop = DateTime.Now.Second;
+            Console.WriteLine("elapsed time is " + (stop - start) + " seconds");
+            Console.ReadKey();
+        }
+    }
+}
